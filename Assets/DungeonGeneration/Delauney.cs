08cccfbcb30442cdc24be37ebd352c7a172bec08
TurkeuBacon <turkeubacon@gmail.com>
2024-04-26 10:20:29 -0700

    Circle Stuff

--- Assets/DungeonGeneration/Delauney.cs
+++ Assets/DungeonGeneration/Delauney.cs
@@ -9,15 +9,24 @@ public class Delauney : MonoBehaviour
         addSuperTriangle(ref graph);
         List<Vector2> unusedPoints = new List<Vector2>(points);
         List<int> triangles = new List<int>();
-        Vector2[] initialVertices = graph.getVertices();
-        triangles.Add(initialVertices.Length-3);
-        triangles.Add(initialVertices.Length-2);
-        triangles.Add(initialVertices.Length-1);
+        Vector2[] graphVerts = graph.getVertices();
+        triangles.Add(graphVerts.Length-3);
+        triangles.Add(graphVerts.Length-2);
+        triangles.Add(graphVerts.Length-1);
 
         while(unusedPoints.Count > 0) {
             int pointIndex = Random.Range(0, unusedPoints.Count);
             Vector2 point = unusedPoints[pointIndex];
             unusedPoints.RemoveAt(pointIndex);
+            for(int i = 0; i < triangles.Count; i+=3) {
+                Circle circumcircle = getCircumcircle(
+                    graphVerts[triangles[i]],
+                    graphVerts[triangles[i+1]],
+                    graphVerts[triangles[i+2]]);
+                if((point-circumcircle.position).magnitude <= circumcircle.radius) {
+                    Debug.Log("IN THE CIRCLE");
+                }
+            }
         }
 
         return graph;
@@ -56,5 +65,28 @@ public class Delauney : MonoBehaviour
         graph.addEdge(a, b);
         graph.addEdge(b, c);
         graph.addEdge(c, a);
+    }
+
+    private static Circle getCircumcircle(Vector2 a, Vector2 b, Vector2 c) {
+        float t = Mathf.Pow(a.x, 2) + Mathf.Pow(a.y, 2) - Mathf.Pow(b.x, 2) - Mathf.Pow(b.y, 2);
+        float u = Mathf.Pow(a.x, 2) + Mathf.Pow(a.y, 2) - Mathf.Pow(c.x, 2) - Mathf.Pow(c.y, 2);
+        float J = (a.x-b.x)*(a.y-c.y)-(a.x-c.x)*(a.y-b.y);
+        
+        float x = (-(a.y-b.y)*u+(a.y-c.y)*t)/(2*J);
+        float y = (-(a.x-b.x)*u+(a.x-c.x)*t)/(2*J);
+        Vector2 position = new Vector2(x, y);
+
+        return new Circle(position, (position-a).magnitude);
+    }
+
+    private struct Circle {
+
+        public Vector2 position;
+        public float radius;
+        public Circle(Vector2 position, float radius) {
+            this.position = position;
+            this.radius = radius;
+        }
+
     }
 }
