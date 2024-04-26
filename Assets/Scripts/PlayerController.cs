e57f89281c64f2ba389895dd762dd5a637c17c92
jkawai35 <jkawai@ucsc.edu>
2024-04-26 13:25:05 -0700

    animations coded, need to fix rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
     Rigidbody2D rb;
+
+    private Animator animator;
+    bool isMoving;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
     }
 
     
@@ -19,18 +27,27 @@ public class PlayerController : MonoBehaviour
     }
 
     void PlayerMovement(){
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        if (!isMoving)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
 
-        Vector2 movement = new Vector2(x,y);
-        movement.Normalize();
+            Vector2 movement = new Vector2(x,y);
+            movement.Normalize();
 
-        rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
-        
-        Vector2 moveDirection = rb.velocity;
-        if (moveDirection != Vector2.zero) {
-        	float angle = Mathf.Atan2(moveDirection.x, -moveDirection.y) * Mathf.Rad2Deg;
-        	transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
+            
+            Vector2 moveDirection = rb.velocity;
+            if (moveDirection != Vector2.zero) {
+                float angle = Mathf.Atan2(moveDirection.x, -moveDirection.y) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                animator.SetFloat("moveX", x);
+                animator.SetFloat("moveY", y);
+                isMoving = true;
+            }
+
+            animator.SetBool("isMoving", isMoving);
         }
+        isMoving = false;
     }
 }
