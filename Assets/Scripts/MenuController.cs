479639de76acfba6951d2db81684e90b314722d3
lmurayam <130516683+lmurayam@users.noreply.github.com>
2024-05-08 16:14:51 -0700

    fixed end menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,11 +8,15 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
     public void ChangeScene(string s){
+        if(text!=null){
+            text.alpha = 0;
+        }
         SceneManager.LoadScene(s);
     }
 
     void Start(){
         if(text!=null){
+            text.alpha = 100;
             int end = PlayerPrefs.GetInt("end",0);
             if(end==0){
                 text.text = "THE MONSTER GOT YOU";
